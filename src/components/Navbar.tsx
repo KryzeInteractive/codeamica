60128f3bc842ff44fc08b4a5b30f466dd6b44b2d
Le Tran Anh Khoi <143178887+Kais4r@users.noreply.github.com>
2024-10-02 10:37:49 +0700

    Clean up navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ export default function Navbar() {
     <div>
       <nav className="border-gray-200 bg-white dark:bg-gray-900">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+          {/* Page logo and name */}
           <Link
             href="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -44,6 +45,7 @@ export default function Navbar() {
               />
             </svg>
           </button>
+
           {/* Navigation */}
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="mt-4 flex select-none flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
@@ -57,7 +59,7 @@ export default function Navbar() {
               </li>
               <li>
                 <Link
-                  href="/courses"
+                  href="/pages/courses"
                   className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
                 >
                   Courses
