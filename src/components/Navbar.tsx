6d6dd839491d34b72695c8e9df6da9e8e76a5be7
Le Tran Anh Khoi <143178887+Kais4r@users.noreply.github.com>
2024-10-09 10:15:04 +0700

    change signout button ui and add style to home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 "use client";
+import { useState } from "react";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
-
 import { SignoutButton } from "./SignoutButton";
 
 export default function Navbar() {
   const { data: session } = useSession();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
   console.log(session);
   if (session === null || session === undefined) {
     console.log("not log in yet");
@@ -75,25 +81,94 @@ export default function Navbar() {
                   Courses
                 </Link>
               </li>
-              <li>
-                <Link
-                  href="/pages/login"
-                  className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-                >
-                  Log in
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/pages/signup"
-                  className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-                >
-                  Sign up
-                </Link>
-              </li>
-              <li>
-                <SignoutButton />
-              </li>
+              {session === null ? (
+                <>
+                  <li>
+                    <Link
+                      href="/pages/login"
+                      className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                    >
+                      Log in
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/pages/signup"
+                      className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                    >
+                      Sign up
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  {/* Dropdown menu */}
+                  <li className="relative">
+                    <button
+                      id="dropdownNavbarLink"
+                      onClick={toggleDropdown}
+                      className="flex w-full items-center justify-between rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:w-auto md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                    >
+                      {session?.user?.name}
+                      <svg
+                        className="ms-2.5 h-2.5 w-2.5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 10 6"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="m1 1 4 4 4-4"
+                        />
+                      </svg>
+                    </button>
+
+                    <div
+                      id="dropdownNavbar"
+                      className={`absolute z-10 ${isDropdownOpen ? "block" : "hidden"} w-44 divide-y divide-gray-100 rounded-lg bg-white font-normal shadow dark:divide-gray-600 dark:bg-gray-700`}
+                    >
+                      <ul
+                        className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                        aria-labelledby="dropdownLargeButton"
+                      >
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >
+                            Dashboard
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >
+                            Settings
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >
+                            Earnings
+                          </a>
+                        </li>
+                      </ul>
+                      <div className="py-1">
+                        <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                          <SignoutButton />
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
