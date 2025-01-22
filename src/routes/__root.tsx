cc209c9c512edee483d42075c8d179a610b68fea
Le Tran Anh Khoi <kais4r.dev@gmail.com>
2025-01-22 10:50:49 +0700

    Revert "Feature/add close btn to login sign up"

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,21 +1,17 @@
-import { createRootRoute, Outlet, useLocation } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 export const Route = createRootRoute({
   component: RootComponent,
 });
-
-const noDefaultLayoutRoutes = ['/login', '/sign-up'];
 function RootComponent() {
-  const location = useLocation()
   return (
-    <>
-      {/* {!noDefaultLayoutRoutes.includes(location.pathname) && <Navbar />} */}
-      <Navbar/>
-      <Outlet />
-      {!noDefaultLayoutRoutes.includes(location.pathname) && <Footer />}
-      <TanStackRouterDevtools position="bottom-right" />
-    </>
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+        <TanStackRouterDevtools position="bottom-right" />
+      </>
   );
 }
